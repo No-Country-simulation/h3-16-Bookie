1fafa1625e684653e4e216d740ac885000016fed
lionel staricoff <staricofflionel@hotmail.com>
2024-11-29 18:20:57 -0500

    error: funcionando la cracion de probincias con paises, el problema es que duplica los datos de los paises

--- Backend/src/main/java/com/Bookie/entities/HistoryEntity.java
+++ Backend/src/main/java/com/Bookie/entities/HistoryEntity.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity(name = "HistoryEntity")
